b7cb30e08f08b9243a21005651c8a80498079503
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-07 14:40:38 +0530

    cloudinary issue is fixed but not accessing from env

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,18 @@
 import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs"
 
-          
 cloudinary.config({ 
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key:process.env.CLOUDINARY_API_KEY, 
-    api_secret:process.env.CLOUDINARY_API_SECRET, 
+  cloud_name: 'dzqgoemai', 
+  api_key: '619485343257381', 
+  api_secret: 'ifeOLEE5PMbiszUvfmf0aDumztc' 
 });
+
+
+// cloudinary.config({ 
+//     cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key:process.env.CLOUDINARY_API_KEY, 
+//     api_secret:process.env.CLOUDINARY_API_SECRET, 
+// });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
@@ -18,6 +24,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         // file has been uploaded successfull
         //console.log("file is uploaded on cloudinary ", response.url);
         // fs.unlinkSync(localFilePath)
+        console.log(`cloud_name:${process.env.CLOUDINARY_CLOUD_NAME}`)
+        console.log(`api_key:${process.env.CLOUDINARY_API_KEY}`)
+        console.log(`api_secret:${process.env.CLOUDINARY_API_SECRET}`)
         return response;
 
     } catch (error) {
