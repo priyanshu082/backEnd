d303a7c2ba0cc6c6ec470748c79a2ecc765457bb
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-08 23:48:05 +0530

    subscription model is done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -23,10 +23,10 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file has been uploaded successfull
         //console.log("file is uploaded on cloudinary ", response.url);
-        // fs.unlinkSync(localFilePath)
-        console.log(`cloud_name:${process.env.CLOUDINARY_CLOUD_NAME}`)
-        console.log(`api_key:${process.env.CLOUDINARY_API_KEY}`)
-        console.log(`api_secret:${process.env.CLOUDINARY_API_SECRET}`)
+         fs.unlinkSync(localFilePath)
+        // console.log(`cloud_name:${process.env.CLOUDINARY_CLOUD_NAME}`)
+        // console.log(`api_key:${process.env.CLOUDINARY_API_KEY}`)
+        // console.log(`api_secret:${process.env.CLOUDINARY_API_SECRET}`)
         return response;
 
     } catch (error) {
